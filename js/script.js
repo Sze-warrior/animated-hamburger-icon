6bc55061217280a1c060863ff345c11c27cafb39
Michael Szeto <szetomj@hotmail.co.nz>
2016-08-17 22:32:54 +1200

    Fixed interaction timing
    Fixed the timing on my 3rd hamburger span to disappear faster.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	$('.menu-btn').click(function(){
 		$('.navbar-list').slideToggle();
-		$('.lvl-3').toggle();
+		$('.lvl-3').toggle('fast');
 		$('.lvl-2').toggleClass('lvl-2-rotated');
 		$('.lvl-1').toggleClass('lvl-1-rotated');
 	});
